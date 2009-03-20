1d9c1ec6d11fa71f3a05af9b4f64c4e0827421d4
Gleb Dolgich <gleb@proggle.com>
2009-03-20 22:23:11 +0000

    Added files to support custom URL scheme for application registration; README updated with instructions

---
+++ objc/URLCommand.h
@@ -0,0 +1,22 @@
+//
+//  CocoaFob
+//
+//  URLCommand.h
+//
+//  Support for custom URL scheme for app registration
+//
+//  Created by Gleb Dolgich on 20/03/2009.
+//  Follow me on Twitter @gbd
+//  Copyright (C) 2009 PixelEspresso. All rights reserved.
+//  Licensed under CC Attribution License 3.0 <http://creativecommons.org/licenses/by/3.0/>
+//
+//  Based on "Handling URL schemes in Cocoa", a blog post by Kimbro Staken
+//  <http://www.xmldatabases.org/WK/blog/1154?t=item>
+//
+
+#import <Foundation/Foundation.h>
+
+@interface URLCommand : NSScriptCommand {
+}
+
+@end
