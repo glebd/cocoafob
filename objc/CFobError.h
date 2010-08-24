14347f8c024d4d966eeeed8df4b88c63780a49a8
Danny Greg <danny@realmacsoftware.com>
2010-08-24 11:26:50 +0100

    Added in a header with an error code enum.

---
+++ objc/CFobError.h
@@ -0,0 +1,17 @@
+//
+//  CFobError.h
+//  cocoafob
+//
+//  Created by Danny Greg on 24/08/2010.
+//  Copyright 2010 Realmac Software. All rights reserved.
+//  Licensed under CC Attribution Licence 3.0 <http://creativecommons.org/licenses/by/3.0/>
+//
+
+
+enum _CFobErrorCode {
+	CFobErrorCodeInvalidKey = -1,
+	CFobErrorCodeCouldNotDecode = -2,
+	CFobErrorCodeSigningFailed = -3,
+	CFobErrorCodeCouldNotEncode = -4,
+	CFobErrorCodeInvalidKey = -5
+};
