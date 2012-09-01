168b4efc25811098e4b0505b39b86b2887823609
Gleb Dolgich <glebd@me.com>
2012-09-01 16:03:02 +0100

    Add skeleton test project

---
+++ objc/CocoaFobTests/CocoaFobTests.m
@@ -0,0 +1,32 @@
+//
+//  CocoaFobTests.m
+//  CocoaFobTests
+//
+//  Created by Gleb on 01/09/2012.
+//
+//
+
+#import "CocoaFobTests.h"
+
+@implementation CocoaFobTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CocoaFobTests");
+}
+
+@end
