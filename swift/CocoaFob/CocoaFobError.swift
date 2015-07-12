fc51e170dc23fe2b19e885f06b7c58b00518862b
Gleb Dolgich <glebd70@gmail.com>
2015-07-12 03:47:46 +0100

    Refine error codes

--- swift/CocoaFob/CocoaFobError.swift
+++ swift/CocoaFob/CocoaFobError.swift
@@ -12,10 +12,20 @@ import Foundation
 Custom error type:
 - InvalidPrivateKey(OSStatus): The supplied private key is invalid; use `security error <OSStatus>` to get the message
 - DecodeError: The supplied key could not be decoded
-- SigningFailed: The cryptographic signing operation resulted in an error
+- InvalidName: Invalid user name supplied
+- ErrorCreatingSignerTransform: Unable to create cryptographic signing transform
+- ErrorConfiguringSignerTransform: Unable to configure signer transform
+- ErrorCreatingEncoderTransform: Unable to create Base32 encoder transform
+- ErrorCreatingGroupTransform: Unable to create group transform
+- ErrorGeneratingRegKey: Unable to generate registration key
 */
 enum CocoaFobError: ErrorType {
   case InvalidPrivateKey(OSStatus)
   case DecodeError
-  case SigningFailed
+  case InvalidName
+  case ErrorCreatingSignerTransform
+  case ErrorConfiguringSignerTransform
+  case ErrorCreatingEncoderTransform
+  case ErrorCreatingGroupTransform
+  case ErrorGeneratingRegKey
 }
