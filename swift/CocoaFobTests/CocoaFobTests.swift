97b9e2281b720fa37598faa3bb85342346518391
Gleb Dolgich <glebd70@gmail.com>
2015-07-12 15:30:39 +0100

    Remove commented-out template code

--- swift/CocoaFobTests/CocoaFobTests.swift
+++ swift/CocoaFobTests/CocoaFobTests.swift
@@ -20,23 +20,6 @@ class CocoaFobTests: XCTestCase {
     + "keUwLHBtpClnD5E8\n"
     + "-----END DSA PRIVATE KEY-----\n"
   
-    /*override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }*/
-    
-    /*override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }*/
-    
-    /*func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }*/
-  
   func testSetPrivateKeyPass() {
     do {
       let keygen = try CocoaFobLicGenerator(privateKeyPEM: privateKeyPEM)
