c470d70b17fbf24f6e70e03ade011227de6b6b7d
Gleb Dolgich <glebd70@gmail.com>
2015-07-14 02:04:49 +0100

    Add new Swift target cocoafob-keygen

---
+++ swift/cocoafob-keygen/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  cocoafob-keygen
+//
+//  Created by Gleb Dolgich on 14/07/2015.
+//  Copyright © 2015 PixelEspresso. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
