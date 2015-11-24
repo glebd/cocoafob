73931135b79ac856b0fff657b30973be4e7f442c
Daniel Alm <Daniel.Alm@ForumD.net>
2015-11-24 08:33:27 +0100

    Add an option to the python server to return previously-generated license keys again.

--- python/license_server.py
+++ python/license_server.py
@@ -9,8 +9,9 @@ import cocoafob
 import sqlite3
 
 app = Flask(__name__)
+app.enable_caching = True
 
-def generate_response(secret, user, email):
+def generate_response(cursor, secret, user, email):
   if secret not in app.secrets:
     return Response(response='Error: Wrong secret.', status=503)
   
@@ -20,25 +21,40 @@ def generate_response(secret, user, email):
   if not email:
     return Response(response='Error: Email is required.', status=400)
 
-  with open(app.private_key_path) as keyfile:
-    return Response(response=cocoafob.make_license(keyfile.read(), app.product, user))
+  # Try to fetch an existing key from the DB.
+  key = None
+  if app.enable_caching:
+    cursor.execute('SELECT response FROM requests WHERE secret = ? AND user = ? AND email = ? ORDER BY date ASC LIMIT 1', [secret, user, email])
+    key_row = cursor.fetchone()
+    if key_row and len(key_row[0]) >= 60:  # Crude check whether the response is a key and not an error message.
+      key = key_row[0].encode('utf-8')
   
-def log_response(secret, user, email, response):
-  db = sqlite3.connect(app.db_path)
-  cursor = db.cursor()
+  if not key:
+    # Need to read the private key and generate the key ourselves.
+    with open(app.private_key_path) as keyfile:
+      key = cocoafob.make_license(keyfile.read(), app.product, user)
+  
+  if key:
+    return Response(response=key)
+  else:
+    return Response(response='Error: error generating the key.', status=500)
+  
+def log_response(cursor, secret, user, email, response):
   cursor.execute('CREATE TABLE IF NOT EXISTS requests '
                  '(date DATETIME, secret TEXT, user TEXT, email TEXT, response TEXT)')
   cursor.execute('INSERT INTO requests VALUES (CURRENT_TIMESTAMP, ?, ?, ?, ?)',
                   [secret, user, email, response.response[0]])
-  db.commit()
 
 @app.route('/', methods=['GET'])
 def return_key():
   secret = request.args.get('secret', '')
   user = request.args.get('user', '')
   email = request.args.get('email', '')
-  response = generate_response(secret, user, email)
-  log_response(secret, user, email, response)
+  db = sqlite3.connect(app.db_path)
+  cursor = db.cursor()
+  response = generate_response(cursor, secret, user, email)
+  log_response(cursor, secret, user, email, response)
+  db.commit()
   return response
 
 if __name__ == '__main__':
