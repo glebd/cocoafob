c344293db19b43a86fa626ad1822fe2dd127d2d6
Christian Tietze <me@christiantietze.de>
2022-12-22 17:16:25 +0100

    bump SPM to Swift 5.5 and macOS 10.13 for the next release

--- Package.swift
+++ Package.swift
@@ -1,10 +1,12 @@
-// swift-tools-version:5.3
+// swift-tools-version: 5.5
 
 import PackageDescription
 
 let package = Package(
     name: "CocoaFob",
-    platforms: [.macOS(.v10_10)],
+    platforms: [
+      .macOS(.v10_13),
+    ],
     products: [
         .library(
             name: "CocoaFob",
